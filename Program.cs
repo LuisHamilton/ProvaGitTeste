d18dfcf6b36dd2e4ce51d91e2876a4ffa64dec73
LuisHamilton <lhbalem@gmail.com>
2023-04-27 20:17:05 -0300

    C4

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@
     public string? Nome { get; set; }
     public int Numero { get; set; }
 }
+
+public class BranchGalho2
+{
+    public string? Funcao { get; set; }
+    public int Posicao { get; set; }
+}
